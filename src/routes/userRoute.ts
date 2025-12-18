50f368ae9bc8f1c09f553e4ae8c12755b06801ae
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-19 01:55:40 +0700

    feat: push

---
+++ src/routes/userRoute.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { userController } from "../controllers/userController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { authorizeRoles } from "../middlewares/roleMiddleware";
+
+
+const router = Router();
+
+// User melihat profil sendiri
+router.get("/profile", authMiddleware, userController.getProfile);
+
+router.get("/", authMiddleware, authorizeRoles("Admin"), userController.getAll);
+
+router.get("/:id", authMiddleware, userController.getById);
+
+router.patch("/:id", authMiddleware, userController.edit);
+
+router.delete("/:id", authMiddleware, authorizeRoles("Admin"), userController.deleteById);
+
+export default router;
