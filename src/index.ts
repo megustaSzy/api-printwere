7ba3e8a04d94e9a76692ad187056c6e2d6131e03
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-22 23:27:54 +0700

    feat: update index

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,42 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import express from "express";
+
+
+import { requestLogger } from "./middlewares/logger";
+
+import userRoute from "./routes/userRoute";
 import authRoute from "./routes/authRoute";
 
 const app = express();
+
 app.use(express.json());
 
+app.use(requestLogger);
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 
 
 app.get("/", (req, res) => {
-    res.json({
-        status: 200,
-        message: "Welcome API"
-    })
+  res.json({
+    status: 200,
+    message: "Welcome Server API",
+    success: true,
+  });
 });
 
-
-app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 
 export default app;
