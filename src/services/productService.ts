017b922f75aa20b482cd1bcad29c1e0a0428a4e6
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-12-23 19:35:26 +0700

    feat: add CRUD productService

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,37 +1,80 @@
-import prisma from "../libs/prisma"
+import prisma from "../libs/prisma";
 import { ProductData } from "../types/product";
 import { createError } from "../utils/createError";
 
-
 export const productService = {
+  async getAll() {
+    return prisma.product.findMany({
+      where: {
+        isActive: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  },
 
-    async findAll() {
-        return prisma.product.findMany({
-            orderBy: {
-                createdAt: "desc"
-            }
-        })
-    },
+  async getProductById(id: number) {
+    const productId = await prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
 
-    async getById(id: number) {
-        const product = await prisma.product.findUnique({
-            where: {
-                id
-            }
-        });
+    if (!productId) throw createError("id tidak ditemukkan", 404);
 
-        if(!product) throw createError("id tidak ditemukkan", 404);
+    return productId;
+  },
 
-        return product;
-    },
+  async createProdct(data: ProductData) {
+    const existingProduct = await prisma.product.findFirst({
+      where: {
+        name: data.name,
+      },
+    });
 
-    async createProducct(data: ProductData) {
-        const product = await prisma.product.findFirst({
-            where: {
-                name: data.name
-            }
-        })
+    if (existingProduct) throw createError("nama sudah ada", 400);
 
-        if(product) throw createError("nama sudah ada", 400)
-    }
-}
+    return prisma.product.create({
+      data: {
+        name: data.name,
+        sku: data.sku,
+        price: data.price,
+        stock: data.stock,
+      },
+    });
+  },
+
+  async editProductById(id: number, data: ProductData) {
+    const productId = await prisma.product.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!productId) throw createError("id tidak ditemukan", 404);
+
+    return prisma.product.update({
+      where: {
+        id,
+      },
+      data,
+    });
+  },
+
+  async deleteProductById(id: number) {
+    const productId = await prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+
+    if (!productId) throw createError("id tidak ditemukan", 404);
+
+    return prisma.product.delete({
+      where: {
+        id,
+      },
+    });
+  },
+};
